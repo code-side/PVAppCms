97af08351b627edfe91ed61cbf6f3590b264b092
Jorge Arguedas <jorge.argds@gmail.com>
2017-08-08 12:53:59 -0600

    Tourist dest issues fixed

--- src/main/webapp/app/entities/tourist-destination/tourist-destination.model.ts
+++ src/main/webapp/app/entities/tourist-destination/tourist-destination.model.ts
@@ -1,14 +1,16 @@
 import { BaseEntity } from './../../shared';
 import { ProvinceRef } from './../province/province-ref.model';
 import { AttributeRef } from './attribute-ref.model';
+import { CoordinatesRef } from './coordinates-ref.model';
+import { PhotoRef } from './photo-ref.model';
 
 export class TouristDestination implements BaseEntity {
     constructor(
         public id?: string,
         public name?: string,
-        public coordinates?: string,
+        public coordinates?: CoordinatesRef,
         public description?: string,
-        public photos?: string[],
+        public photos?: PhotoRef[],
         public address?: string,
         public province?: ProvinceRef,
         public attributes?: AttributeRef[],
